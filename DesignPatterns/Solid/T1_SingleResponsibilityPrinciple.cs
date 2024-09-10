2b551aa66991504cd69b90b4a1cc80644ad512cb
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-10 21:03:24 +0200

    Setup for Single Responsibility Principle done

---
+++ DesignPatterns/Solid/T1_SingleResponsibilityPrinciple.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPatterns.Solid;
+
+public class T1_SingleResponsibilityPrinciple
+{
+    public static void Demo()
+    {
+        var journal = new Journal();
+        journal.AddEntry("I cried today");
+        journal.AddEntry("I ate a bug");
+        Console.WriteLine(journal);
+    }
+
+    public class Journal
+    {
+        private readonly List<string> entries = [];
+        private static int count = 0;
+
+        public int AddEntry(string entry)
+        {
+            entries.Add($"{++count}: {entry}");
+            return count;
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+}

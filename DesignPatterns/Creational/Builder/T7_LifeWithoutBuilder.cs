ca26710929fc836904d2eda98e522fd5c88a4cf7
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-12 08:14:04 +0200

    Example of HTML builder with low-level API added

---
+++ DesignPatterns/Creational/Builder/T7_LifeWithoutBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace DesignPatterns.Creational.Builder;
+
+public class T7_LifeWithoutBuilder
+{
+    public static void Demo()
+    {
+        var text = "hello";
+        var sb = new StringBuilder();
+        sb.Append("<p>");
+        sb.Append(text);
+        sb.Append("</p>");
+
+        List<string> words = ["Hello", "world"];
+        sb.Clear();
+        sb.Append("<ul>");
+        foreach (var word in words)
+        {
+            sb.AppendFormat("<li>{0}</li>", word);
+        }
+
+        sb.Append("</ul>");
+        WriteLine(sb);
+    }
+}

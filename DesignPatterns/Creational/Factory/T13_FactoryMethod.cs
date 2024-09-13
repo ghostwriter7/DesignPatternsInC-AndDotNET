c562a3d5d88b43ae67d52e8d98c6a48fa6f8844a
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-13 14:51:27 +0200

    Factory Method demoed

---
+++ DesignPatterns/Creational/Factory/T13_FactoryMethod.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns.Creational.Factory;
+
+public class T13_FactoryMethod
+{
+    public static void Demo()
+    {
+    }
+
+    public class Point
+    {
+        private double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static Point NewPolarPoint(double x, double y)
+        {
+            return new Point(x, y);
+        }
+
+        public static Point NewCartesianPoint(double rho, double theta)
+        {
+            return NewPolarPoint(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+    }
+}

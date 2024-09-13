e4667ad13262b173da2a3218c0035a54e9517835
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-13 15:00:38 +0200

    Async Factory Method demoed

---
+++ DesignPatterns/Creational/Factory/T14_AsyncFactoryMethod.cs
@@ -0,0 +1,29 @@
+﻿namespace DesignPatterns.Creational.Factory;
+
+public class T14_AsyncFactoryMethod
+{
+    public static async Task Demo()
+    {
+        var foo = await Foo.CreateAsync();
+    }
+
+    public class Foo
+    {
+        private Foo()
+        {
+            
+        }
+
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+
+        public static Task<Foo> CreateAsync()
+        {
+            var foo = new Foo();
+            return foo.InitAsync();
+        } 
+    }
+}

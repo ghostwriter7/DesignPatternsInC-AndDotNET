30c73dc07b12a55294c1907f83667f02e4043bae
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-13 20:51:42 +0200

    Private ctor and inner factory

---
+++ DesignPatterns/Creational/Factory/T16_InnerFactoryForPrivateConstructor.cs
@@ -0,0 +1,28 @@
+﻿namespace DesignPatterns.Creational.Factory;
+
+public class T16_InnerFactoryForPrivateConstructor
+{
+    public static void Demo()
+    {
+        Point.Factory.NewCartesianPoint(5, 10);
+    }
+
+    public class Point
+    {
+        public double x, y;
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y) => new Point(x, y);
+
+            public static Point NewPolarPoint(double rho, double theta) =>
+                new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+    }
+}

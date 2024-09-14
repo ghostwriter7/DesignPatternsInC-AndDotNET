2f8f5d566480b6d9b56cb27671059c6cd0a52b7b
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-14 13:07:47 +0200

    DI with AutoFac

--- DesignPatterns/Creational/Singleton/T22_SingletonDatabase.cs
+++ DesignPatterns/Creational/Singleton/T22_SingletonDatabase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Xml.Serialization;
+using Autofac;
 using NUnit.Framework;
 
 namespace DesignPatterns.Creational.Singleton.SingletonDatabase;
@@ -14,6 +15,35 @@ public class T22_SingletonDatabase
 public interface IDatabase
 {
     int GetPopulation(string cityName);
+}
+
+public class OrdinaryDatabase : IDatabase
+{
+    private Dictionary<string, int> _cities;
+
+    public OrdinaryDatabase()
+    {
+        WriteLine("Reading the database...");
+
+        _cities = File.ReadAllLines(
+                Path.Combine(
+                    new FileInfo(typeof(SingletonDatabase).Assembly.Location).DirectoryName,
+                    "capitals.txt"
+                ))
+            .Select((line) =>
+            {
+                var elements = line.Split(':');
+                return (capital: elements.ElementAt(0), population: int.Parse(elements.ElementAt(1).Replace(",", "")));
+            }).ToDictionary(
+                (element => element.capital),
+                (element => element.population)
+            );
+    }
+
+    public int GetPopulation(string cityName)
+    {
+        return _cities[cityName];
+    }
 }
 
 public class SingletonDatabase : IDatabase
@@ -59,6 +89,25 @@ public class RecordFinder
         names.Select((name) => SingletonDatabase.Instance.GetPopulation(name)).Sum();
 }
 
+public class DummyDatabase : IDatabase
+{
+    public int GetPopulation(string cityName)
+    {
+        return new Dictionary<string, int>()
+        {
+            ["alpha"] = 1,
+            ["beta"] = 2,
+            ["gamma"] = 3
+        }[cityName];
+    }
+}
+
+public class ConfigurableRecordFinder(IDatabase database)
+{
+    public int GetTotalPopulation(IEnumerable<string> names) =>
+        names.Aggregate(0, (total, name) => total + database.GetPopulation(name));
+}
+
 [TestFixture]
 class SingletonTests
 {
@@ -78,5 +127,27 @@ class SingletonTests
         var total = new RecordFinder().GetTotalPopulation(names);
         // problem! running tests on live database due to singleton's implementation
         Assert.That(total, Is.EqualTo(20463000 + 9541000));
+    }
+
+    [Test]
+    public void ConfigurableRecordFinderTest()
+    {
+        var crf = new ConfigurableRecordFinder(new DummyDatabase());
+        Assert.That(crf.GetTotalPopulation(["alpha", "beta"]), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void DIOrdinaryDatabaseTest()
+    {
+        var cb = new ContainerBuilder();
+        cb.RegisterType<OrdinaryDatabase>()
+            .As<IDatabase>()
+            .SingleInstance();
+        cb.RegisterType<ConfigurableRecordFinder>();
+        using (var ctx = cb.Build())
+        {
+            var configurableRecordFinder = ctx.Resolve<ConfigurableRecordFinder>();
+            Assert.That(configurableRecordFinder.GetTotalPopulation(["Tokyo"]), Is.EqualTo(37393000));
+        }
     }
 }

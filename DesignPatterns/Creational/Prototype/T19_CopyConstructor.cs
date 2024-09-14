cbd4776483ce1096b6c06ac8f87bae917b773ec8
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-14 10:48:07 +0200

    Copy Constructor

---
+++ DesignPatterns/Creational/Prototype/T19_CopyConstructor.cs
@@ -0,0 +1,40 @@
+﻿namespace DesignPatterns.Creational.Prototype;
+
+public class T19_CopyConstructor
+{
+    public class Person
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public Person(Person other)
+        {
+            Names = other.Names;
+            Address = new Address(other.Address);
+        }
+    }
+
+    public class Address
+    {
+        public string Street;
+        public int HouseNumber;
+
+        public Address(string street, int houseNumber)
+        {
+            Street = street;
+            HouseNumber = houseNumber;
+        }
+
+        public Address(Address other)
+        {
+            Street = other.Street;
+            HouseNumber = other.HouseNumber;
+        }
+    }
+}

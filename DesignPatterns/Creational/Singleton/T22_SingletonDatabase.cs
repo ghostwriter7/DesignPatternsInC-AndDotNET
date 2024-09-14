29a88cccc74997c6e507ba723375151bf181370d
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-14 12:28:32 +0200

    Issues in testing singletons demoed

--- DesignPatterns/Creational/Singleton/T22_SingletonDatabase.cs
+++ DesignPatterns/Creational/Singleton/T22_SingletonDatabase.cs
@@ -1,4 +1,5 @@
 ﻿using System.Xml.Serialization;
+using NUnit.Framework;
 
 namespace DesignPatterns.Creational.Singleton.SingletonDatabase;
 
@@ -22,12 +23,20 @@ public class SingletonDatabase : IDatabase
     private static Lazy<SingletonDatabase> instance = new Lazy<SingletonDatabase>(() => new SingletonDatabase());
 
     public static SingletonDatabase Instance = instance.Value;
-    
+
+    private static int instanceCount;
+    public static int InstanceCount => instanceCount;
+
     public SingletonDatabase()
     {
         WriteLine("Reading the database...");
+        instanceCount++;
 
-        _cities = File.ReadAllLines(@"Creational\Singleton\capitals.txt")
+        _cities = File.ReadAllLines(
+                Path.Combine(
+                    new FileInfo(typeof(SingletonDatabase).Assembly.Location).DirectoryName,
+                    "capitals.txt"
+                ))
             .Select((line) =>
             {
                 var elements = line.Split(':');
@@ -41,5 +50,33 @@ public class SingletonDatabase : IDatabase
     public int GetPopulation(string cityName)
     {
         return _cities[cityName];
+    }
+}
+
+public class RecordFinder
+{
+    public int GetTotalPopulation(IEnumerable<string> names) =>
+        names.Select((name) => SingletonDatabase.Instance.GetPopulation(name)).Sum();
+}
+
+[TestFixture]
+class SingletonTests
+{
+    [Test]
+    public void IsSingletonTest()
+    {
+        var db1 = SingletonDatabase.Instance;
+        var db2 = SingletonDatabase.Instance;
+        Assert.That(db1, Is.SameAs(db2));
+        Assert.That(SingletonDatabase.InstanceCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void GetTotalPopulationTest()
+    {
+        string[] names = ["Beijing", "London"];
+        var total = new RecordFinder().GetTotalPopulation(names);
+        // problem! running tests on live database due to singleton's implementation
+        Assert.That(total, Is.EqualTo(20463000 + 9541000));
     }
 }

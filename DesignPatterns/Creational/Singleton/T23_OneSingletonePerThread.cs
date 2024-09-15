db40d8e9250144a794f10abe004ac96851af3da2
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-15 09:59:10 +0200

    ThreadLocal<T>

---
+++ DesignPatterns/Creational/Singleton/T23_OneSingletonePerThread.cs
@@ -0,0 +1,34 @@
+﻿namespace DesignPatterns.Creational.Singleton.SingletonDatabase;
+
+public class T23_OneSingletonePerThread
+{
+    public static void Demo()
+    {
+        var t1 = Task.Factory.StartNew(() =>
+        {
+            WriteLine($"T{Task.CurrentId}: {SingletonPerThread.ThreadInstance.Id}");
+        });
+        var t2 = Task.Factory.StartNew(() =>
+        {
+            WriteLine($"T{Task.CurrentId}: {SingletonPerThread.ThreadInstance.Id}");
+            WriteLine($"T{Task.CurrentId}: {SingletonPerThread.ThreadInstance.Id}");
+        });
+
+        Task.WaitAll(t1, t2);
+    }
+}
+
+public class SingletonPerThread
+{
+    private static readonly ThreadLocal<SingletonPerThread> _threadLocal =
+        new(() => new SingletonPerThread());
+
+    public static SingletonPerThread ThreadInstance => _threadLocal.Value;
+    public int Id { get; private set; }
+    
+    private SingletonPerThread()
+    {
+        WriteLine($"Constructor called by {Task.CurrentId}");
+        Id = Task.CurrentId.Value;
+    }
+}

3021c095d1c5491a6147a04d7817475f3d21c6d3
ghostwriter7 <hubert.rozplochowski@gmail.com>
2024-09-15 12:34:21 +0200

    Recursive generics introduced to avoid constructor generation via a Factory Method on base class

--- DesignPatterns/Structural/Adapter/T26_GenericValueAdapter.cs
+++ DesignPatterns/Structural/Adapter/T26_GenericValueAdapter.cs
@@ -5,13 +5,12 @@
 /// </summary>
 public class T26_GenericValueAdapter
 {
-
     public static void Demo()
     {
-        var vec2a = new Vector2i(0, 100);
-        var vec2b = new Vector2i(50, 2);
+        var vec2a = Vector2i.Create(0, 100);
+        var vec2b = Vector2i.Create(50, 2);
         var vec3 = new Vector3f(3, 5, 10);
-        
+
         WriteLine($"Vec2a: {vec2a}");
         WriteLine($"Vec2b: {vec2b}");
         WriteLine($"Vec2a + Vec2b: {vec2a + vec2b}");
@@ -37,9 +36,11 @@ public static class Dimensions
     }
 }
 
-public abstract class Vector<T, D> where D : IInteger, new()
+public abstract class Vector<TSelf, T, D> 
+    where D : IInteger, new()
+    where TSelf : Vector<TSelf, T, D>, new()
 {
-    private readonly T[] _data;
+    private T[] _data;
 
     protected Vector(params T[] values)
     {
@@ -51,7 +52,20 @@ public abstract class Vector<T, D> where D : IInteger, new()
         for (var i = 0; i < Math.Min(requiredSize, providedSize); i++)
             _data[i] = values[i];
     }
-    
+
+    public static TSelf Create(params T[] values)
+    {
+        var self = new TSelf();
+        var requiredSize = new D().Value;
+        self._data = new T[requiredSize];
+
+        var providedSize = values.Length;
+
+        for (var i = 0; i < Math.Min(requiredSize, providedSize); i++)
+            self._data[i] = values[i];
+        return self;
+    }
+
     public T this[int index]
     {
         get => _data[index];
@@ -67,37 +81,36 @@ public abstract class Vector<T, D> where D : IInteger, new()
 /// <summary>
 /// Specialized class (intermediate class in hierarchy) to provide operator overloading
 /// </summary>
-public class VectorOfInts<D>(params int[] values) : Vector<int, D>(values) where D : IInteger, new()
+public class VectorOfInts<TSelf, D>(params int[] values) : Vector<TSelf, int, D>(values)
+    where D : IInteger, new()
+    where TSelf : VectorOfInts<TSelf, D>, new()
 {
     protected VectorOfInts() : this([])
     {
     }
 
-    public static VectorOfInts<D> operator +(VectorOfInts<D> lhs, VectorOfInts<D> rhs)
+    public static VectorOfInts<TSelf, D> operator +(VectorOfInts<TSelf, D> lhs, VectorOfInts<TSelf, D> rhs)
     {
-        var vector = new VectorOfInts<D>();
+        var vector = new VectorOfInts<TSelf, D>();
         for (int i = 0; i < new D().Value; i++)
             vector[i] = lhs[i] + rhs[i];
         return vector;
     }
 
-    public static VectorOfInts<D> operator -(VectorOfInts<D> lhs, VectorOfInts<D> rhs)
+    public static VectorOfInts<TSelf, D> operator -(VectorOfInts<TSelf, D> lhs, VectorOfInts<TSelf, D> rhs)
     {
-        var vector = new VectorOfInts<D>();
+        var vector = new VectorOfInts<TSelf, D>();
         for (int i = 0; i < new D().Value; i++)
             vector[i] = lhs[i] - rhs[i];
         return vector;
     }
 }
 
-public class Vector2i(params int[] values) : VectorOfInts<Dimensions.Two>(values)
+public class Vector2i : VectorOfInts<Vector2i, Dimensions.Two>
 {
-    public Vector2i() : this([])
-    {
-    }
 }
 
-public class Vector3f(params float[] values) : Vector<float, Dimensions.Three>(values)
+public class Vector3f(params float[] values) : Vector<Vector3f, float, Dimensions.Three>(values)
 {
     public Vector3f() : this([])
     {
